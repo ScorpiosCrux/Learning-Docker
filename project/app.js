ab85d724ef17c32d0acae38f6c33f6e73b596a45
Tyler Chen <chentyler52@gmail.com>
2022-12-23 05:07:49 -0700

    we are now able to save data in the docker container

--- project/app.js
+++ project/app.js
@@ -1,20 +1,43 @@
-const express = require('express')
-var bodyParser = require('body-parser');
-const app = express()
-const port = 3000
+const express = require("express");
+var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const app = express();
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-	res.sendFile('index.html', {root: __dirname })
-})
+// Set up default mongoose connection
+mongoose.connect("mongodb://localhost:27017/learningDocker", { authSource: "admin", user: "admin", pass: "password" });
+// const test1 = mongoose.connection;
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.post('/', (req, res) => {
-	const name = req.body
-	res.send(`Got your response!`)
-})
+//Basic Schema
+const NameSchema = new Schema({
+	name: String,
+});
+
+// Compile model from schema
+const NameModel = mongoose.model("NameModel", NameSchema);
+
+app.get("/", (req, res) => {
+	res.sendFile("index.html", { root: __dirname });
+});
+
+app.post("/", (req, res) => {
+	const newName = req.body.name;
+
+	const nameInstance = new NameModel({ name: newName });
+
+	// Save the new model instance, passing a callback
+	nameInstance.save((err) => {
+		if (err) console.log("failed to save");
+	});
+	return res.send(`Name has been saved!`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
