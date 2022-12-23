5a4b36663bd084e1d9f234d88b3a515684ac5a73
Tyler Chen <chentyler52@gmail.com>
2022-12-23 04:00:38 -0700

    added simple form and post request

--- project/app.js
+++ project/app.js
@@ -1,9 +1,18 @@
 const express = require('express')
+var bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+	res.sendFile('index.html', {root: __dirname })
+})
+
+app.post('/', (req, res) => {
+	const name = req.body
+	res.send(`Got your response!`)
 })
 
 app.listen(port, () => {
